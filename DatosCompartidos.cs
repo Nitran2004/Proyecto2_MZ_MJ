fae5685c10161d11bb93222553f9e692cc8393ee
Nitran2004 <93067795+Nitran2004@users.noreply.github.com>
2023-11-27 08:33:02 -0500

    1commit, Datos compartidos

---
+++ DatosCompartidos.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Proyecto2_MZ_MJ
+{
+    public static class DatosCompartidos
+    {
+        public static string Valor1 { get; set; }
+    }
+
+}
